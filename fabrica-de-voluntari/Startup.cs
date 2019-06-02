860c9ad4a17710e4c78d0c20e190f73cf6d6c7ba
Adrian Panaintescu <adrian.cert@gmail.com>
2019-06-02 12:02:37 +0300

    Main Project Created

---
+++ fabrica-de-voluntari/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fabrica_de_voluntari.Startup))]
+namespace fabrica_de_voluntari
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
