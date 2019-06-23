fac2fa8a8017cb0033b4db093449d9a34ef48b0b
Adrian Panaintescu <adrian.cert@gmail.com>
2019-06-24 02:02:24 +0300

    DataBase Update

---
+++ Data/DataBase/DataBaseConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data
+{
+    using System;
+    using System.Data.Entity;
+    using System.Collections.Generic;
+    using System.Linq;
+
+
+    public class DataBase : DbContext
+    {
+        public DataBase()
+            : base("name=DataBase")
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserIndentification> UserIndentifications { get; set; }
+        public DbSet<UserContact> UserContacts { get; set; }
+        public DbSet<UserMail> UserMails { get; set; }
+        public DbSet<UserNumber> UserNumbers { get; set; }
+        public DbSet<UserSocial> Socials { get; set; }
+        public DbSet<Adress> Adresses { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
