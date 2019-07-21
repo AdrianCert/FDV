327ccc3d7423d3ec72e7de042835e080ba8750ba
Adrian Panaintescu <adrian.cert@gmail.com>
2019-07-22 01:06:23 +0300

    add missing file

---
+++ src/Data/DataBase/UserIndentification.cs
@@ -0,0 +1,18 @@
+﻿namespace Data
+{
+    using System;
+    using System.Data.Entity;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.ComponentModel.DataAnnotations;
+    public class UserIndentification
+    {
+        public Guid Id { get; set; }
+        public string Serial { get; set; }
+        public int Number { get; set; }
+        public int CNP { get; set; }
+        public Guid UserID { get; set; }
+        [Required]
+        public virtual User User { get; set; }
+    }
+}
