4bb29bd8df65fb7bc478a5d50dc8db2b2ef713b0
Adrian Panaintescu <adrian.cert@gmail.com>
2019-09-24 08:16:44 +0300

    reable

---
+++ src/fabrica-de-voluntari/Areas/Gfest/GfestAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace fabrica_de_voluntari.Areas.Gfest
+{
+    public class GfestAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Gfest";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Gfest_default",
+                "Gfest/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/fabrica-de-voluntari/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fabrica_de_voluntari.MvcApplication" Language="C#" %>
