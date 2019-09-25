94ef955f6b6a537695091c093a6433d197a00b5d
Adrian Panaintescu <adrian.cert@gmail.com>
2019-09-25 20:40:55 +0300

    Add Users Page

---
+++ src/fabrica-de-voluntari/Areas/Gfest/Controllers/UserController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Data;
+
+namespace fabrica_de_voluntari.Areas.Gfest.Controllers
+{
+    public class UserController : Controller
+    {
+        private Gfest2019 db = new Gfest2019();
+
+        // GET: Gfest/User
+        public ActionResult Index()
+        {
+            return View(db.Participanti.ToList());
+        }
+
+        // GET: Gfest/User/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Participant participant = db.Participanti.Find(id);
+            if (participant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participant);
+        }
+
+        // GET: Gfest/User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gfest/User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nume,Prenume,IsMan,Age,Email,Telefon,Facebook,AlteRetele,Status,Abonat,PassWord")] Participant participant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Participanti.Add(participant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(participant);
+        }
+
+        // GET: Gfest/User/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Participant participant = db.Participanti.Find(id);
+            if (participant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participant);
+        }
+
+        // POST: Gfest/User/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nume,Prenume,IsMan,Age,Email,Telefon,Facebook,AlteRetele,Status,Abonat,PassWord")] Participant participant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(participant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(participant);
+        }
+
+        // GET: Gfest/User/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Participant participant = db.Participanti.Find(id);
+            if (participant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participant);
+        }
+
+        // POST: Gfest/User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Participant participant = db.Participanti.Find(id);
+            db.Participanti.Remove(participant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
